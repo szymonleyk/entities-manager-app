8446cbf8d91ef15698ff4d3ca9d6999eda7dab49
Szymon Leyk <biuro@szymonleyk.pl>
2023-06-06 18:23:28 +0200

    add menu component, and user data

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list density="compact" nav>
+        <v-list-item
+          :prepend-avatar="require('./assets/profile.png')"
+          title="Szymon Leyk"
+          nav
+        ></v-list-item>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Entities Manager</v-toolbar-title>
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App',
-
-  data () {
-    return {
-      //
-    }
-  },
-})
+<script>
+export default {
+  data: () => ({ drawer: null }),
+}
 </script>
