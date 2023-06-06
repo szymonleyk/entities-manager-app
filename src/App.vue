d381ffdf36bcad0bf718dd0c74576d7ae015dab0
Szymon Leyk <biuro@szymonleyk.pl>
2023-06-06 19:26:55 +0200

    #5 add profile and dashboard menu items and routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <v-list density="compact" nav>
-        <v-list-item
-          :prepend-avatar="require('./assets/profile.png')"
-          title="Szymon Leyk"
-          nav
-        ></v-list-item>
-        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item :prepend-avatar="require('./assets/profile.png')" title="Szymon Leyk" nav></v-list-item>
       </v-list>
+
+      <v-list-item v-for="item in items" :key="item.title" :prepend-icon="item.icon" :title="item.title"
+        :value="item.value" :to="item.to" link>
+      </v-list-item>
     </v-navigation-drawer>
 
     <v-app-bar>
@@ -19,13 +17,20 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Dashboard', value: 'dashboard', icon: 'mdi-view-dashboard', to: '/' },
+      { title: 'Profile', value: 'profile', icon: 'mdi-account', to: '/profile' },
+      { title: 'Logout', value: 'logout', icon: 'mdi-logout-variant', to: '/logout' }
+    ]
+  }),
 }
 </script>
