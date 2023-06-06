0e2769cadeccc329cc3a43d8716b499ce2238231
Szymon Leyk <biuro@szymonleyk.pl>
2023-06-07 00:26:00 +0200

    #7 communicate with backend application, get all and remove

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -3,70 +3,71 @@
     <thead>
       <tr>
         <th class="text-left" v-for="item in headers">
-          {{item}}
+          {{ item }}
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
+      <tr v-for="item in customers" :key="item.name">
         <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.vat }}</td>
+        <td>
+          <v-icon size="small" class="me-2" @click="editItem(item.id)">
+            mdi-pencil
+          </v-icon>
+          <v-icon size="small" @click="deleteItem(item.id)">
+            mdi-delete
+          </v-icon>
+        </td>
       </tr>
     </tbody>
   </v-table>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        headers: ['Name', 'Calories'],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
-      }
+import axios from 'axios';
+export default {
+
+  data() {
+    return {
+      headers: ['Name', 'E-mail', 'Vat', 'Actions'],
+      customers: [],
+    }
+  },
+
+  created() {
+    axios.get(`http://localhost:3333/api/v1/customers`)
+      .then(response => {
+        console.log(response.data);
+        this.customers = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  },
+
+  methods: {
+    editItem(id) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(id) {
+      console.log(id)
+      axios.delete(`http://localhost:3333/api/v1/customers/${id}`)
+        .then((response) => {
+          console.log(`Customer with ID ${id} deleted successfully`);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(`Error deleting customer with ID ${id}`);
+          console.error(error);
+        });
     },
   }
+
+
+}
 </script>
